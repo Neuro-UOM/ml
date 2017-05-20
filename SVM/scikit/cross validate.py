eeb1938c79bd2ebcf6087cea4e67c4793f715317
dinukasal <dinukasal@gmail.com>
2017-05-20 11:42:17 +0530

    cross validation with scikit code added

---
+++ SVM/scikit/cross validate.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 20 11:27:23 2017
+
+@author: dinuka
+"""
+
+from sklearn.externals import joblib
+clf = joblib.load('svm_model.pkl') 
+
+import pandas as pd
+
+location='/home/dinuka/fyp/ml_git_repo/Data/train.csv'
+df=pd.read_csv(location,header=None)
+
+arr=df.as_matrix()
+
+from scipy.fftpack import fft
+y=fft(df)
+
+#print clf.predict(y[3700:3836,:14])
+
+
+#output=clf.predict(y[-200:,:14])
+
+from sklearn import metrics
+from sklearn.model_selection import cross_val_predict
+predicted = cross_val_predict(clf, y[:,:14], arr[:,-1], cv=10)
+print metrics.accuracy_score(arr[:,-1], predicted) 
