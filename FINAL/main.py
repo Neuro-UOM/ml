abc73632d9a57f280bc55be3e5999e3d422f4f93
chathuranga94 <chathuranga94@live.com>
2017-05-28 16:04:43 +0530

    model classifier saved

--- FINAL/main.py
+++ FINAL/main.py
@@ -1,6 +1,5 @@
 from importer import csvImporter
 from abstract_MLAlgo import MLAlgo
-
 from scikit_NeuralNetwork import scikit_NeuralNetwork
 from scikit_NaiveBayes import scikit_NaiveBayes
 from scikit_DecisionTree import scikit_DecisionTree
@@ -8,7 +7,8 @@ from scikit_SupportVectorMachine import scikit_SupportVectorMachine
 from scikit_KNearestNeighbor import scikit_KNearestNeighbor
 from scikit_NearestCentroid import scikit_NearestCentroid
 from scikit_StochasticGradientDescent import scikit_StochasticGradientDescent
-from scikit_GaussianProcessClassifier import scikit_GaussianProcessClassifier
+#from scikit_GaussianProcessClassifier import scikit_GaussianProcessClassifier
+from sklearn.externals import joblib
 
 train_data = csvImporter('train.csv').getData()    
 test_data = csvImporter('test.csv').getData()
@@ -27,7 +27,18 @@ for ML_Class in MLAlgo.__subclasses__():
     print(MLmodel.predict(train_data[100:101,:-1]))
     print(MLmodel.cross_validate(train_data))
 
+    joblib.dump(MLmodel.clf, ML_Class.__name__ + '.pkl') 
+    print("Classifier model saved to ", ML_Class.__name__ , ".pkl\n")
+    #loadClassifier = joblib.load(ML_Class.__name__ + '.pkl') 
+
 input("Press Enter to continue...")
+
+'''
+import pickle
+s = pickle.dumps(MLmodel.clf)
+loadClassifier = pickle.loads(s)
+print(loadClassifier.predict(train_data[100:101,:-1]))
+'''
 
 
 '''
